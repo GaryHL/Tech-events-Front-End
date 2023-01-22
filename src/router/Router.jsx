89111ca50d630834010aaac2fc82ce8e41d7c8ae
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-22 21:47:01 +0100

    feat: :white_check_mark: Create new route Search
    Create new route and call view search

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../views/home/Home";
+import Search from "../views/search/Search";
 
 const Router = () => {
    return (
@@ -12,6 +13,7 @@ const Router = () => {
                element={<Home/>}
             />
             <Route path="/home" element={<Home/>} />
+            <Route path="/search" element={<Search/>} />
          </Routes>
       </BrowserRouter>
    );
